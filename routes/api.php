0ac70197c0578fcc7fb1fd412c12342d2a0dfe33
DavidTimoteus <2131710012@student.polinema.ac.id>
2024-11-12 11:39:39 +0700

    fix routing

--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\UploadController;
+use App\Http\Controllers\Admin\ForumController as AdminForumController;
 use App\Http\Controllers\Admin\UniversityController as AdminUniversityController;
 
 Route::get('/', function () {
@@ -15,7 +16,8 @@ Route::prefix('home')->name('home.')->group(function () {
 });
 Route::prefix('admin')->name('admin.')->group(function () {
     Route::get('/universities', [AdminUniversityController::class, 'list']);
+    Route::get('/forums', [AdminForumController::class, 'list']);
     // Route::prefix('admin')->name('admin.')->middleware(['auth:jwt'])->group(function () {
 });
 Route::post('/upload', [UploadController::class, 'upload']);
-Route::get('/forums', [ForumController::class, 'list']);
+
