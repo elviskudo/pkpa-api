df5d311f5d2f71ef469a28db486e485c96c7284e
FelixYuda <yuda8675@gmail.com>
2025-01-03 00:51:03 +0700

    Controller University

--- app/Http/Controllers/Admin/UniversityController.php
+++ app/Http/Controllers/Admin/UniversityController.php
@@ -10,16 +10,43 @@ class UniversityController extends Controller
 {
     public UniversityServices $universityServices;
 
-    public function __construct (UniversityServices $universityServices)
+    public function __construct(UniversityServices $universityServices)
     {
-        parent::__construct();
         $this->universityServices = $universityServices;
     }
 
-    public function list (Request $request)
+    public function list(Request $request)
     {
-        $universities = $this->universityServices->list();
+        $universities = $this->universityServices->list($request);
 
-        return json_encode($universities);
+        return response()->json($universities, 200);
+    }
+
+   
+    public function create(Request $request)
+    {
+        $data = $request->validate();
+
+        $university = $this->universityServices->create($data);
+
+        return response()->json($university, 201);
+    }
+
+    
+    public function update(Request $request, $id)
+    {
+        $data = $request->validate();
+
+        $university = $this->universityServices->update($id, $data);
+
+        return response()->json($university, 200);
+    }
+
+   
+    public function delete($id)
+    {
+        $this->universityServices->delete($id);
+
+        return response()->json(null, 204);
     }
 }
