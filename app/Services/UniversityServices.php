1815d904ed01b27273ce03ad919eeb7278af8594
FelixYuda <yuda8675@gmail.com>
2025-01-03 00:50:45 +0700

    Services University

--- app/Services/UniversityServices.php
+++ app/Services/UniversityServices.php
@@ -4,39 +4,33 @@ namespace App\Services;
 
 use Illuminate\Http\Request;
 use App\Models\University;
-use Illuminate\Database\Eloquent\Model;
 
 class UniversityServices
 {
-    public $addressFilter = ['name', 'code'];
-
-    /**
-     * @throws Exception
-     */
     public function list(Request $request)
     {
-        $model = University::all();
-
-        return $model;
+        return University::all();
     }
 
+    
     public function create($data)
     {
-        $model = University::create($data);
-
-        return $model;
+        return University::create($data);
     }
 
+   
     public function update($id, $data)
     {
-        $model = University::createOrUpdate($data, $id);
+        $model = University::findOrFail($id);
+        $model->update($data);
 
         return $model;
     }
 
     public function delete($id)
     {
-        $model = University::delete($id);
+        $model = University::findOrFail($id);
+        $model->delete();
 
         return $model;
     }
